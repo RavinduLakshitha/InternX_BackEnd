23a0e61f487179de2e461fba9238517bd97c7b10
ravindu <ravindulakshitha250@gmail.com>
2024-06-05 17:00:28 +0530

    Updated mongodb.js and created collection

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = 3000;
-const DB_URL =
-  "mongodb+srv://ravi123:Abc123@cluster0.fbt0o.mongodb.net/internX";
+const DB_URL = 'mongodb+srv://ravi123:Abc123@cluster0.fbt0o.mongodb.net/internX';
 
-mongoose
-  .connect(DB_URL)
-  .then(() => {
-    console.log("DB connected");
-  })
-  .catch((err) => console.log("DB connection error"));
+mongoose.connect(DB_URL)
+.then(() =>{
+  console.log('DB connected');
+})
+.catch((err) => console.log('DB connection error'))
 
-app.listen(PORT, () => {
-  console.log("App is running on ${3000}");
-});
+app.listen(PORT, () =>{
+  console.log('App is running on ${3000}');
+})
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose")
 
+mongoose.Collect("mongodb://localhost:27017/internX")
+.then(() =>{
+  console.log('mongodb connected');
+})
+.catch((err) => console.log('DB connection error'))
+
 const LogInShema = new mongoose.Schema({
   name:{
     type:String,
@@ -13,4 +19,4 @@ const LogInShema = new mongoose.Schema({
 
 const collection = new mongoose.model("Collection1",LogInShema)
 
-module.exports = collection
+module.exports = collection;
