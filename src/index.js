38740e001dca75af003758704f2695d2abe2707b
ravindu <ravindulakshitha250@gmail.com>
2024-06-05 18:46:01 +0530

    update structure

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-const hbs = require("hbs")
-const collection = require("./mongodb")
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const collection = require("./mongodb");
 
-const templatePath = path.join(__dirname,'../tempelates')
+const templatePath = path.join(__dirname, "../tempelates");
 
-app.use(express.json())
-app.set("view engine","hbs")
-app.set("views",templatePath)
-app.use(express.urlencoded({extended:fals}))
+app.use(express.json());
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+app.use(express.urlencoded({ extended: fals }));
 
-app.get("/",(req,res)=>{
-  res.render("login")
-})
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
-app.post("/login",async (req,res) => {
-  const data={
-    name:req.body.name,
-    password:req.body.password
+app.post("/login", async (req, res) => {
+  const data = {
+    name: req.body.name,
+    password: req.body.password,
+  };
 
-  }
-
-  await collection.insertMany([data])
-})
+  await collection.insertMany([data]);
+});
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,22 +1,23 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.Collect("mongodb://localhost:27017/internX")
-.then(() =>{
-  console.log('mongodb connected');
-})
-.catch((err) => console.log('DB connection error'))
+mongoose
+  .Collect("mongodb://localhost:27017/internX")
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => console.log("DB connection error"));
 
 const LogInShema = new mongoose.Schema({
-  name:{
-    type:String,
-    required:true
+  name: {
+    type: String,
+    required: true,
   },
-  password:{
-    type:String,
-    required:true
-  }
-})
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-const collection = new mongoose.model("Collection1",LogInShema)
+const collection = new mongoose.model("Collection1", LogInShema);
 
 module.exports = collection;
