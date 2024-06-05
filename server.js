f8510288f5376a7d45fd33dd7ae4c47731515bde
ravindu <ravindulakshitha250@gmail.com>
2024-06-05 15:23:48 +0530

    Connect Database

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const port = 3000;
+const PORT = 3000;
+const DB_URL = 'mongodb+srv://ravi123:Abc123@cluster0.fbt0o.mongodb.net/internX';
 
-app.listen(port, () =>{
+mongoose.connect(DB_URL)
+.then(() =>{
+  console.log('DB connected');
+})
+.catch((err) => console.log('DB connection error'))
+
+app.listen(PORT, () =>{
   console.log('App is running on ${3000}');
 })
