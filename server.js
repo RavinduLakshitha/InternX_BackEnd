3b84aad239204bf0678c53fd1cad706b0f4a468d
kushana <rashmisenadheera98@gmail.com>
2024-06-07 21:42:50 +0530

    Connected to the frontend

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express');
-
+const express = require("express");
+const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 4000;
+const data_product = require("./src/data");
 
-const port = 3000;
+app.use(cors()); // Allow requests from any origin
 
-app.listen(port, () =>{
-  console.log('App is running on ${3000}');
-})
+// Define the route to serve data
+app.get("/displayData", (req, res) => {
+  res.json(data_product); // Send data_product directly
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
