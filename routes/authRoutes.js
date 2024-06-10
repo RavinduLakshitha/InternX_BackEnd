384b1d9cf3389f116daae656fd78c4bc578bbb32
ravindu <ravindulakshitha250@gmail.com>
2024-06-10 17:58:28 +0530

    create authRoutes file

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const cors = require('../controllers/authController.js')
+const cors = require("../controllers/authController.js");
+router.post("/create", authController.createUser);
 
-router.use(
-  cors({
-    Credentials: true,
-    origin: 'https;//localhost:5173'
-  })
-)
-
-router.get('/', test)
-router.post('/login', loginUser)
-
-module.exports = router
+module.exports = router;
