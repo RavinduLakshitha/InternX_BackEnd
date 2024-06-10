fc8fb7144c6aa0ec1ce5a31a36396addcbb2bba4
ravindu <ravindulakshitha250@gmail.com>
2024-06-10 17:56:51 +0530

    Create controller file for user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,13 @@
-const User = require('../models/user')
+const User = require("../models/user");
 
 exports.createuser = async (req, res) => {
   const name = req.body.email;
-  const password = req.body.password;
+  // const password = req.body.password;
 
-  const newUser = new User({ name, address});
+  const newUser = new User({ name, address });
 
   const savedUser = await newUser.save();
   res.status(201).json({
-    message: 'User login successfully'
+    message: "User login successfully",
   });
-
-}
+};
