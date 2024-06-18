350b2dd97ea6da3e6f1fefbcd8dd46919835a664
kushana <rashmisenadheera98@gmail.com>
2024-06-19 01:34:44 +0530

    Created Addvertisment Schema

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const { mongoose } = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const advertisementRoutes = require('./routes/advertismentRoutes');
+const data_product = require('./data');
 
 const app = express();
-app.use(express.json());
+const port = process.env.PORT || 4000;
 
-const PORT = 8000;
+app.use(cors());
+app.use(bodyParser.json());
 
-mongoose
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB connected");
-  })
-  .catch((err) => console.log("DB connection error"));
+mongoose.connect('mongodb+srv://kushana:Izn5o0iC865jB8yg@cluster0.erqc8h4.mongodb.net/ReactCardInternX?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
 
-app.listen(PORT, () => {
-  console.log(`App is running on ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
-
-const userRoute = require("../routes/authRoutes");
-app.use("/user", userRoute);
