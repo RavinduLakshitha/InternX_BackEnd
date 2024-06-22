f5b710ffdabfaed11ba804bcae14bfb5a9b3d897
kushana <rashmisenadheera98@gmail.com>
2024-06-22 16:03:56 +0530

    Set get request in advertismentController

--- src/routes/advertismentRoutes.js
+++ src/routes/advertismentRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const advertisementController = require('../controllers/advertismentController');
 
 router.post('/submit-add', advertisementController.submitAdvertisement);
+router.get('/display-add'),
 
 module.exports = router;
