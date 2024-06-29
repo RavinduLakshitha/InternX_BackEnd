0af8ebab5b86b93aaea765e071a1bf37d48f4ef3
ravindu <ravindulakshitha250@gmail.com>
2024-06-30 00:27:11 +0530

    updated registerduser controller file

--- src/controllers/registerdUsercontroller.js
+++ src/controllers/registerdUsercontroller.js
@@ -7,18 +7,18 @@ exports.login = async (req, res) => {
   const newUser = new RegisterdUser({ email, password });
 
   try {
-    const user = await newUser.findOne({ email });
+    const user = await RegisterdUser.findOne({ email });
 
     if (!user) {
       return res.status(400).send("Invalid email or password.");
     }
-
     if (user.password !== password) {
       return res.status(400).send("Invalid email or password.");
     }
 
     res.status(200).send("Login successfully!");
   } catch (err) {
+    console.error("Error during login:", err);
     res.status(500).send("Login not complete.");
   }
 };
