089d7d26a6282f11d5e888a217e792823dcc4cb2
kushana <rashmisenadheera98@gmail.com>
2024-07-02 23:07:54 +0530

    set path to delete

--- src/routes/advertismentRoutes.js
+++ src/routes/advertismentRoutes.js
@@ -5,6 +5,6 @@ const advertisementController = require('../controllers/advertismentController')
 router.post('/submit-add', advertisementController.submitAdvertisement);
 router.get('/display-add',advertisementController.displayAdvetisements);
 router.get('/display-add/:id', advertisementController.getAdvertisementById);
-router.get('/delete-add/:id', advertisementController.deleteAdvertismentById);
+router.delete('/delete-add/:id', advertisementController.deleteAdvertismentById);
 
 module.exports = router;
